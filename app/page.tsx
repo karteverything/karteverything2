d699fce97275771a84d463f1486be1f12367fb70
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-03-07 15:47:14 +0200

    Build home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-
+import Header from "../compontents/Header";
+import Hero from "../compontents/Hero";
+import About from "../compontents/About";
+import Services from "../compontents/Services";
+import Gallery from "../compontents/Gallery";
+import Blog from "../compontents/Blog";
+import Contact from "../compontents/Contact";
+import Footer from "../compontents/Footer";
 
 export default function Home() {
-  
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <Gallery />
+      <Blog />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
