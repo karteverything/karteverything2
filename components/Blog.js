4034b631aa2498b443facf177cfd06b01025ad29
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-03-08 14:29:07 +0200

    edit blog section

---
+++ components/Blog.js
@@ -0,0 +1,48 @@
+import Link from "next/link";
+
+const posts = [
+    {
+        title: "Personal Growth",
+        snippet: "This page is a snapshot of what I'm currently learning, building, and improving.",
+        slug: "Projects",
+    },
+    {
+        title: "Supabase Integration",
+        snippet: "See how I integrated Supabase database for my Portraiture Gallery.",
+        slug: "Supabase",
+    },
+    {
+        title: "The Precision of Perfection",
+        snippet: "The say perfection is a sickness, but maybe it's just love expressed with precision.",
+        slug: "Perfection",
+    },
+    {
+        title: "Engineering Emotion: Why I love cars",
+        snippet: "From roaring engines to sleek designs, cars aren't just machines.",
+        slug: "Cars",
+    },
+];
+
+export default function Blog() {
+    <div className="blog-container" container>
+
+        <div className="blog-title">
+            <h2>Latest Blog Posts</h2>
+            <p>Thoughts, tutorials, insight, and stories behind my work.</p>
+        </div>
+
+        <div className="blog-grid">
+
+            {posts.map((post, index) => (
+                <div className="blog-card" key={index}>
+                    <h3>{post.title}</h3>
+                    <p className="blog-snippet">{post.snippet}</p>
+
+                    <Link href={`/blog/${post.slug}`} className="btn">
+                        Read More
+                    </Link>
+                </div>
+            ))}
+        </div>
+    </div>
+}
