e018e89d50d9c3db84856a0b1b430cd2c1cf00e8
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-03-10 21:24:19 +0200

    fix rendering

--- components/Blog.js
+++ components/Blog.js
@@ -24,25 +24,29 @@ const posts = [
 ];
 
 export default function Blog() {
-    <div className="blog-container" container>
+    return (
+        <section id="blog" className="blog-section">
+            <div className="blog-container container">
 
-        <div className="blog-title">
-            <h2>Latest Blog Posts</h2>
-            <p>Thoughts, tutorials, insight, and stories behind my work.</p>
-        </div>
-
-        <div className="blog-grid">
-
-            {posts.map((post) => (
-                <div className="blog-card" key={post.href}>
-                    <h3>{post.title}</h3>
-                    <p className="blog-snippet">{post.snippet}</p>
-
-                    <Link href={`/blog/${post.href}`} className="btn">
-                        Read More
-                    </Link>
+                <div className="blog-title">
+                    <h2>Latest Blog Posts</h2>
+                    <p>Thoughts, tutorials, insight, and stories behind my work.</p>
                 </div>
-            ))}
-        </div>
-    </div>
+
+                <div className="blog-grid">
+
+                    {posts.map((post) => (
+                        <div className="blog-card" key={post.href}>
+                            <h3>{post.title}</h3>
+                            <p className="blog-snippet">{post.snippet}</p>
+
+                            <Link href={post.href} className="btn">
+                                Read More
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
