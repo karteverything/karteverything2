eeb6ad91efc8ecd037888f13d9c46de32400aac0
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-03-10 22:14:16 +0200

    add content

---
+++ app/blog/cars/page.jsx
@@ -0,0 +1,49 @@
+export default function Cars() {
+    return (
+        <section className="blog-post">
+            <div className="blog-post-container">
+
+                <h1 className="engineering">
+                Engineering Emotion: Why I Love Cars
+                </h1>
+
+                <div className="dates-container">
+                <p>Published: 13-05-2025</p>
+                <p>Last Edited: Nov 3, 2025</p>
+                </div>
+
+                <article>
+
+                <p>
+                    From roaring engines to sleek design, cars aren't just machines —
+                    they're reflections of passion, culture, and identity.
+                </p>
+
+                <br />
+
+                <p>
+                    Each curve tells a story. From the aerodynamic grace of a supercar
+                    to the rugged lines of an off-roader, design choices reveal purpose
+                    and personality. They're not simply about performance — they're
+                    emotional blueprints that spark desire, nostalgia, even rebellion.
+                </p>
+
+                <br />
+
+                <p>
+                    More than transport, cars are time capsules. They're reminders of
+                    family road trips, late-night drives, or the first moment you felt
+                    freedom behind the wheel. We don't just build them — we build
+                    around them: communities, memories, dreams.
+                </p>
+
+                </article>
+
+                <a href="/#blog" className="btn">
+                Back to Blog
+                </a>
+
+            </div>
+            </section>
+    );
+}
