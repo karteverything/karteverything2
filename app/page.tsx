cc392a5da2e7ea6eed9ee8a238ea3bd480b4b139
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-03-11 21:24:50 +0200

    add gallery content

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Services from "../components/Services";
-/*import Gallery from "../components/Gallery";*/
+import Gallery from "../components/Gallery";
 import Blog from "../components/BlogPreview";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
@@ -14,7 +14,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
-      {/*}<Gallery />{*/}
+      <Gallery />
       <Blog />
       <Contact />
     </>
---
+++ components/Gallery.jsx
@@ -0,0 +1,20 @@
+export default function Gallery() {
+    return (
+        <section id="portraiture" className="gallery">
+            <div className="gallery-title container">
+                <h2>Portraiture</h2>
+
+                <p className="sunset-quote">
+                    Whether it's a random street corner or a glowing sky, I take
+                    photos to remember how thing felt - the laughter, the stillness
+                    the golden light before sunsets. Nature's quiet masterpieces!
+                </p>
+                
+                {/*}
+                <Slideshow />
+                <Portraiture />
+                {*/}
+            </div>
+        </section>
+    );
+}
