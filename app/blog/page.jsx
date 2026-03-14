c6eb08bd38ac1b8e5401c2714119ef5ccb92bf78
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-03-14 16:35:55 +0200

    fix blog

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,23 +1,29 @@
 import Link from "next/link";
 import { blogPosts } from "../../data/blogPosts";
 
-export default function BlogPage() {
+export default function BlogPreview() {
     return (
-        <section className="blog-container container">
-            <div className="blog-title">
-                <h2>Blog</h2>
-                <p>Thoughts, tutorials, insights, and stories behind my work.</p>
-            </div>
+        <section id="blog" className="blog-section">
+            <div className="blog-container container">
 
-            <div className="blog-grid">
-                {blogPosts.map((post) => (
-                <div key={post.slug} className="blog-card">
-                    <h3>{post.title}</h3>
-                    <p className="blog-snippet">{post.snippet}</p>
-                    <p className="blog-date">Published: {post.published}</p>
-                    <Link href={post.path} className="btn">Read Article</Link>
+                <div className="blog-title">
+                    <h2>Latest Blog Posts</h2>
+                    <p>Thoughts, tutorials, insight, and stories behind my work.</p>
                 </div>
-                ))}
+                
+                <div className="blog-grid">
+
+                    {blogPosts.map((post) => (
+                        <div className="blog-card" key={post.slug}>
+                            <h3>{post.title}</h3>
+                            <p className="blog-snippet">{post.snippet}</p>
+
+                            <Link href={post.path} className="btn">
+                                Read More
+                            </Link>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     );
