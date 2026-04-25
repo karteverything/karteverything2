3a3471058f4b152a6a327dc6c052413d7b3806aa
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-04-25 18:19:26 +0200

    fix prop typing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ReactNode } from "react";
 
 export const metadata = {
     title: "KArt Everything",
     description: "Kat's portfolio and journal",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ 
+    children,
+}: {
+    children: ReactNode;
+}) {
     return (
         <html lang="en">
             <body>
