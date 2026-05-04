fc314e5f1ed272a934157a7b5e5626cd4f0895b2
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-05-04 17:02:37 +0200

    add to admin page routes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
         <footer className="footer-container">
             <div className="bottom-bar">
 
-                <a href="">
+                <a href="/admin">
                     &copy; KArt Everything
                 </a>
 
