ba3f9d76d652f108ba9468e77679ec1c507459a7
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-05-05 12:36:52 +0200

    fix: e.close func for timer

--- app/admin/hooks/useAutoLogout.js
+++ app/admin/hooks/useAutoLogout.js
@@ -3,33 +3,32 @@
 import { useEffect } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
-import { clearTimeout } from "timers";
 
 export default function useAutoLogout() {
-    const router = useRouter();
-    const AUTO_LOGOUT_TIME = 15 * 60 * 1000; // 15 minutes
+  const router = useRouter();
+  const AUTO_LOGOUT_TIME = 15 * 60 * 1000;
 
-    useEffect(() => {
-        let logoutTimer;
+  useEffect(() => {
+    let logoutTimer;
 
-        const resetTimer = () => {
-            clearTimeout(logoutTimer);
+    const resetTimer = () => {
+      clearTimeout(logoutTimer);
 
-            logoutTimer = setTimeout(async () => {
-                alert("Session expired due to inactivity.");
-                
-                await supabase.auth.signOut();
+      logoutTimer = setTimeout(async () => {
+        alert("Session expired due to inactivity.");
+        await supabase.auth.signOut();
+        router.push("/admin");
+      }, AUTO_LOGOUT_TIME);
+    };
 
-                router.push("/admin");
-            }, AUTO_LOGOUT_TIME)
-        };
+    ["click", "mousemove", "keydown", "scroll", "touchstart"].forEach((evt) =>
+      document.addEventListener(evt, resetTimer)
+    );
 
-        ["click", "mousemove", "keydown", "scroll", "touchstart"].forEach((evt) => 
-            document.addEventListener(evt, resetTimer)
-        );
+    resetTimer();
 
-        return () => {
-            clearTimeout(logoutTimer);
-        };
-    }, [router]);
+    return () => {
+      clearTimeout(logoutTimer);
+    };
+  }, [router]);
 }
