c4d0976127db94150933a7f1b62eaca5975ea2ea
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-05-05 11:48:43 +0200

    fix import mismatch

--- app/admin/components/SessionGuard.jsx
+++ app/admin/components/SessionGuard.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/lib/supabaseClient";
+import supabase from "@/lib/supabaseClient";
 import useAutoLogout from "../hooks/useAutoLogout";
 
 export default function SessionGuard({ children }) {
