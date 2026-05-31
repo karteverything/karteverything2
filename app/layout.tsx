99b629265ccf384c0fd47165957ec04d5e655abc
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-05-31 12:41:23 +0200

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { ReactNode } from "react";
 import LayoutContent from "@/components/LayoutContent";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata = {
     title: "KArt Everything",
@@ -19,6 +20,8 @@ export default function RootLayout({
                     {children}
                 </LayoutContent>
             </body>
+
+            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!}/>
         </html>
     );
 }
