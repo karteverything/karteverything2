3cb3743083070ee7436585c6b0d7f997c86652d6
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-06-09 18:26:59 +0200

    add safeguard

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* anti-click-jacking protection */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
