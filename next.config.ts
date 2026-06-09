5f1747ee6f4ebe254672c7c4bfe23ff993492302
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2026-06-09 18:30:29 +0200

    add safeguard

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
             key:"X-Content-Type-Options",
             value:"nosniff",
           },
+          {
+            key:"Referrer-Policy",
+            value:"strict-origin-when-cross-origin",
+          },
         ],
       },
     ];
