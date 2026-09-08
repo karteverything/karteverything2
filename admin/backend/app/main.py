323e4e264055250496a49da373fc8fe3708a476b
Mlungisi <sibitanemlungisi26@gmail.com>
2026-09-08 14:23:56 +0200

    add cors for admin

--- admin/backend/app/main.py
+++ admin/backend/app/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_origins=[
         settings.frontend_url,
         "http://localhost:5173",
+        "https://karteverything-admin.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
